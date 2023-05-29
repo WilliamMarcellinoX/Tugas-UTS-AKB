16883c4af2128730c01b540bc455957e568ff47b
WilliamMarcellinoX <mpartoleksono@gmail.com>
2023-05-30 06:03:15 +0700

    UTS_AKB_10120008
    Tugas Pengganti uts akb

---
+++ app/src/main/java/com/uts/william_10120008/NoteModel.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * Nama : Mahendra Nugraha
+ * Nim : 10120015
+ * Kelas : IF 1
+ * Email : mahenugrahaa
+ *
+ */
+
+package com.uts.william_10120008;
+
+public class NoteModel {
+    int id;
+    String title, description, category, created_at, updated_at;
+}
---
+++ app/src/main/java/com/uts/william_10120008/ui/information/InformationsFragment.java
@@ -0,0 +1,43 @@
+/**
+ *
+ * Nama : Mahendra Nugraha
+ * Nim : 10120015
+ * Kelas : IF 1
+ * Email : mahenugrahaa
+ *
+ */
+
+package com.uts.william_10120008.ui.information;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.uts.william_10120008.databinding.FragmentInformationBinding;
+
+public class InformationsFragment extends Fragment {
+
+    private FragmentInformationBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        InformationsViewModel ViewModel =
+                new ViewModelProvider(this).get(InformationsViewModel.class);
+
+        binding = FragmentInformationBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
